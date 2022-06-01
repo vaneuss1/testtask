65f81b122a5d69e63e6d83938782c4a16f23c4a4
vaneuss1 <issemenov99@mail.ru>
2022-06-01 13:35:01 +0300

    encoding fix

--- change_id_and_name_7_8_9.py
+++ change_id_and_name_7_8_9.py
@@ -5,13 +5,14 @@ from sys import argv
 
 def reverse_id(file_name):
     root = ET.parse(os.path.abspath(file_name))
+    encodings = 'utf-8'
     reverse_ids = list(reversed([tag.get('id') for tag in root.findall('image')]))
     for tag in root.findall('image'):
         new_format = 'png'
         tag.attrib['name'] = f"{tag.attrib['name'].split('/')[-1].split('.')[0]}.{new_format}"
         tag.attrib['id'] = reverse_ids[0]
         del reverse_ids[0]
-    root.write(f"{file_name.split('.')[0]}_new.xml")
+    root.write(f"{file_name.split('.')[0]}_new.xml", encoding='utf-8')
 
 
 if __name__ == '__main__':
